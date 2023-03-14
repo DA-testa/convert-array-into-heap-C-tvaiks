779c7bb309d387a8781fdc9bed1e3b8c9f3fb57b
Mareks Siņica-Siņavskis <90405278+C-tvaiks@users.noreply.github.com>
2023-03-14 17:09:01 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,36 +1,56 @@
 # python3
+# Mareks Siņica-Siņavskis 221RDB430
 
-
-def build_heap(data):
+def build_heap(data,n):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    def Veidot_koku(i):
+        keisais_berns = 2 * i + 1
+        labais_berns = 2 * i + 2
+        mazakais = i
 
+        if keisais_berns < n and data[keisais_berns] < data[mazakais]:
+            mazakais = keisais_berns
+        if labais_berns < n and data[labais_berns] < data[mazakais]:
+            mazakais = labais_berns
+        if mazakais != i:
+            data[i], data[mazakais] = data[mazakais], data[i]
+            swaps.append((i, mazakais))
+            Veidot_koku(mazakais)
+
+    for i in range(n // 2 - 1, -1, -1):
+        Veidot_koku(i)
 
     return swaps
 
 
 def main():
-    
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    input_type = input()
+    if input_type[:1] == 'F':
+        file_name = input()
+        try:
+            with open("test/"+file_name+"") as text_file:
+                n = int(text_file.readline())
+                data = text_file.readline()
+                data = list(map(int, data.split()))
+        except IOError:
+            print('Invalid file name')
+            return
+    elif input_type[:1] == 'I':
+        n = int(input())
+        data = list(map(int, input().split()))
+    else:
+        print('Invalid input!')
+        return
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
     # calls function to assess the data 
     # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
+    swaps = build_heap(data,n)
 
     # output all swaps
     print(len(swaps))
